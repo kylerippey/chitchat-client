54ce06714ad8ce287844c671728a60ef33a99204
Kyle Rippey <kyle@avvo.com>
2013-05-02 10:50:30 -0700

    More class stuff.

--- lib/chitchat-client/chat.rb
+++ lib/chitchat-client/chat.rb
@@ -12,12 +12,20 @@ module Chitchat
           return nil unless response.status == 200 && response.headers['content-type'] == "application/json" && response[:chat]
 
           chat = response[:chat]
-          status = chat[:status]
-          messages = chat[:messages]
+          Chitchat::Client.new(chat[:chat_id], chat[:status], chat[:messages])
+        end
+
+        # POST /chats?from=[my_user_id]&to=[their_user_id]
+        def create(from_user_id, to_user_id)
+          response = Chitchat::Client.connection.post("/chats.json", {:from => from_user_id, :to => to_user_id})
         end
       end
 
-      def initialize
+      def initialize(chat_id, status, messages=[])
+        @chat_id = chat_id
+        @status = status
+        @messages = messages
+        self
       end
 
       # PUT /chats/:id/answer
@@ -30,7 +38,12 @@ module Chitchat
 
       # POST /calls/:id/messages?from=[user_id]&body=[text]
       def send_message(from_id, body)
+        response = Chitchat::Client.connection.post("/calls/#{chat_id}", {:from => from_id, :body => body})
       end
+
+      private
+
+      def 
     end
   end
 end
